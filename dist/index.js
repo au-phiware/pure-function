29455dd599e52178baa7b233c8f3115818d43227
Darren Seet <darren_seet@oredi.com>
2017-07-18 14:41:09 -0400

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,5 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.validator = undefined;
-
 var _validator = require('./validator');
 
-exports.validator = _validator.validator;
+module.exports = _validator.validator;
