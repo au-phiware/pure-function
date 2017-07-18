a17440077ce5e15a0751cf4b4b8a7c60e9e8eedb
Darren Seet <darren_seet@oredi.com>
2017-07-18 14:47:58 -0400

    Change export again

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,8 @@
 
 var _validator = require('./validator');
 
-module.exports = _validator.validator;
+var validator = _interopRequireWildcard(_validator);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+module.exports = validator;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import {validator} from './validator';
+import * as validator from './validator';
 
 module.exports = validator;
